1ff8ddd7e1070eec4d33b1aca791f11bd390253a
fabiocolletto <fabiocolletto@gmail.com>
2023-12-17 22:25:04 -0300

    inicio da personalizacao estrutura de pastas montada, componentes instalados.

---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Feel free to reach out to us for any inquiries or questions.</p>
+      {/* Add a contact form or additional contact information */}
+    </div>
+  );
+}
+
+export default Contact;
